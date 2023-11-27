ea7f42c57bd07c81861d50a4939faf56b994e67a
Ashish Rai <ashish.rai@appinventiv.com>
2023-11-27 19:09:06 +0530

    Added Google Dependency

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:info_profile_ui/repository/firebase_api.dart';
 
 class AuthServices {
@@ -5,5 +7,24 @@ class AuthServices {
     bool? res;
     res =  await FirebaseApi().isLogin();
     return res;
+  }
+}
+
+
+class GoogleAuthService {
+  // Google SignIn
+  Future<UserCredential> signInWithGoogle() async {
+    // Sign begins
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Get Authentication details
+    final GoogleSignInAuthentication gAuth = await googleUser!.authentication;
+
+    // Create credetials for the user
+    final cred = GoogleAuthProvider.credential(
+        accessToken: gAuth.accessToken, idToken: gAuth.idToken);
+
+    // Now let's sign in
+    return await FirebaseAuth.instance.signInWithCredential(cred);
   }
 }
